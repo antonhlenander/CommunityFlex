3ac5264c97805800baadd2badb11e8c19fbe7ac3
ahle <ahle@itu.dk>
2025-02-13 12:59:53 +0100

    Save

--- eval.py
+++ eval.py
@@ -263,9 +263,9 @@ elif sys.argv[1] == "rollout":
             {
                 aid : StrategicProsumerAgent.Supertype(
                     #capacity = UniformIntSampler(1, 4),
-                    eta=0,
+                    eta=0.2,
                     rollout=1,
-                    price_multiplier=2,
+                    price_multiplier=1,
                     maxbuy=1,
                     maxsell=1
                 )    
@@ -285,10 +285,10 @@ elif sys.argv[1] == "rollout":
 
 
     results = ph.utils.rllib.rollout(
-        directory="~/ray_results/single_policy_new/eta0.0/",
+        directory="~/ray_results/single_policy_new/eta0.2_24hobs_diffnorm/",
         #directory="~/ray_results/community_flex_singlepolicy/PPO_StackelbergRewardDelayEnv_2024-12-17_10-29-59a6rem3ul/",
         #directory='~/ray_results/single_policy_new/PPO_StackelbergRewardDelayEnv_2025-02-05_11-42-134musycil',
-        checkpoint=39,
+        #checkpoint=39,
         env_class=StackelbergRewardDelayEnv,
         env_config={
             'num_steps': NUM_EPISODE_STEPS,
@@ -306,7 +306,7 @@ elif sys.argv[1] == "rollout":
 
     results = list(results)
 
-    path = f"output/single_policy_new_maxpricetest/"
+    path = f"output/single_policy_new_eta0.2_24hobs_diffnorm/"
     if not os.path.exists(path):
         os.makedirs(path)
 
